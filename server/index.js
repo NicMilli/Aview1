0ff9255aba3688f76f6281459898bc783f412a16
NicMilli <nicbass2@gmail.com>
2023-07-06 12:53:05 -0700

    Test deployment AWS

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 const path = require('path');
+
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
-
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
