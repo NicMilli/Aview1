497764d5590ce8d8a05fd059fefe1bedfb6dac40
NicMilli <nicbass2@gmail.com>
2023-07-06 12:32:21 -0700

    Test deployment 3

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const path = require('path');
 
-
 const express = require('express');
 const morgan = require('morgan');
 
@@ -13,11 +12,10 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, '../client/dist/')});
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 })
 
 const PORT = process.env.PORT || 3000;
