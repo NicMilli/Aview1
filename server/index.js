f7724d3b0fa17784aa30158d0a6eacfe9b210a3a
NicMilli <nicbass2@gmail.com>
2023-07-06 12:35:27 -0700

    Test deployment 4

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../dist')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
